59161299ff9857e12481141393f255bcb512e013
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 13:53:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 
-st.title('🎈 Survey chatbot')
+st.title("Echo Bot")
 
-st.write('Hello world!')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'How do you feel today? Also, for monitoring purposes, can I ask what is your name, age and sex?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
