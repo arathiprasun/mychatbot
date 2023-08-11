f52c76c393f0e5f22fbeaede85dcb390875279fd
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 12:40:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.title('🎈 Survey chatbot')
 
 st.write('Hello world!')
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
