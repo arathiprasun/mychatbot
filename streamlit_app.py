ae80897438736400e45a473f3113f238ae5791f8
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 13:54:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,4 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
+    
