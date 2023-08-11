9fcffb5e6e74c78e7d569c1859aeb197dbd7fb5a
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 13:00:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,4 @@
-#import streamlit as st
 
-#st.title('🎈 Survey chatbot')
-
-#st.write('Hello world!')
-
-#with st.form('my_form'):
-  #text = st.text_area('Enter text:', 'How do you feel today? Also, for monitoring purposes, can I ask what is your name, age and sex?')
-  #submitted = st.form_submit_button('Submit')
-  #if not openai_api_key.startswith('sk-'):
-    #st.warning('Please enter your OpenAI API key!', icon='⚠')
-  #if submitted and openai_api_key.startswith('sk-'):
-    #generate_response(text)
 import streamlit as st
 from langchain.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
